7644dbb5535f9ddf245949bc42cdfc9ccd20d471
artartur <artur.souza@tds.company>
2025-04-29 15:03:25 -0300

    feat(header) create navbar component

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { HeaderBannerComponent } from './header-banner/header-banner.component';
 import { HeaderBrandComponent } from './header-brand/header-brand.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     HeaderBannerComponent,
     HeaderBrandComponent,
+    NavbarComponent,
   ],
   imports: [
     CommonModule
