70798efd3162384ade7a98ec0325c3ccd96a00b5
artartur <artur.souza@tds.company>
2025-04-29 14:28:00 -0300

    feat(header): create header-brand component

---
+++ src/app/components/header/header-brand/header-brand.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-brand',
+  standalone: false,
+  templateUrl: './header-brand.component.html'
+})
+export class HeaderBrandComponent {
+
+}
--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { HeaderBannerComponent } from './header-banner/header-banner.component';
+import { HeaderBrandComponent } from './header-brand/header-brand.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     HeaderBannerComponent,
+    HeaderBrandComponent,
   ],
   imports: [
     CommonModule
